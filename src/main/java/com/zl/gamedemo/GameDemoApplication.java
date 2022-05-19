79fc7bcca96f0f76aa0cbd56f32ac0b5c321d2a8
xx <xx@xxdeMacBook-Pro.local>
2022-05-19 11:24:18 +0800

    init project

---
+++ src/main/java/com/zl/gamedemo/GameDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zl.gamedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zl/gamedemo/GameDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zl.gamedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
