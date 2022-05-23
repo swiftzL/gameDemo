6908e60030f11a2142ef88a493448dd812cb74ac
xx <xx@xxdeMacBook-Pro.local>
2022-05-23 14:16:26 +0800

    delete service

--- server/src/main/java/com/zl/server/play/base/service/AccountService.java
+++ server/src/main/java/com/zl/server/play/base/service/AccountService.java
@@ -1,24 +1,8 @@
 package com.zl.server.play.base.service;
 
-
-import com.zl.server.anno.NetMessageHandler;
-import com.zl.server.anno.NetMessageInvoke;
-import com.zl.server.cache.EntityCache;
-import com.zl.server.commons.Command;
-import com.zl.server.commons.Response;
-import com.zl.server.dao.AccountDao;
-import com.zl.server.dto.AccountDto;
-
-import com.zl.server.model.Account;
 import com.zl.server.netty.NetConnection;
 import com.zl.server.play.base.packet.MR_Response;
 import com.zl.server.play.base.packet.MS_Account;
-import io.netty.channel.Channel;
-import io.netty.util.Attribute;
-import io.netty.util.AttributeKey;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Service;
 
 
 public interface AccountService {
